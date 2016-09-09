0c5c99a3dc518140a6d8c1858381b0cfe568cdd7
Paul Wagener <mail@paulwagener.nl>
2016-09-09 19:42:10 +0200

    Nice

--- 3-Trees/BinaryTree.cs
+++ 3-Trees/BinaryTree.cs
@@ -8,75 +8,72 @@ namespace ALGA
 {
     public class BinaryTree
     {
-        public class BST
+        Node root;
+
+        /**
+            * Inserts a number into the tree
+            */
+        public void insert(int number)
         {
-            Node root;
+            throw new NotImplementedException();
+        }
 
-            /**
-             * Inserts a number into the tree
-             */
-            public void insert(int number)
-            {
-                throw new NotImplementedException();
-            }
+        /**
+            * Delete a number from the tree
+            */
+        public void delete(int number)
+        {
+            throw new NotImplementedException();
+        }
 
-            /**
-             * Delete a number from the tree
-             */
-            public void delete(int number)
-            {
-                throw new NotImplementedException();
-            }
+        public bool exists(int number)
+        {
+            throw new NotImplementedException();
+        }
 
-            public bool exists(int number)
-            {
-                throw new NotImplementedException();
-            }
+        /**
+            * Returns the smallest value in the tree (or -1 if tree is empty)
+            */
+        public int min()
+        {
+            throw new NotImplementedException();
+        }
 
-            /**
-             * Returns the smallest value in the tree (or -1 if tree is empty)
-             */
-            public int min()
-            {
-                throw new NotImplementedException();
-            }
+        /**
+            * Returns the largest value in the tree (or -1 if tree is empty)
+            */
+        public int max()
+        {
+            throw new NotImplementedException();
+        }
 
-            /**
-             * Returns the largest value in the tree (or -1 if tree is empty)
-             */
-            public int max()
-            {
-                throw new NotImplementedException();
-            }
+        /**
+            * Returns how many levels deep the deepest level in the tree is
+            * (the empty tree is 0 levels deep, the tree with only one root node is 1 deep)
+            * @return
+            */
+        public int depth()
+        {
+            throw new NotImplementedException();
+        }
 
-            /**
-             * Returns how many levels deep the deepest level in the tree is
-             * (the empty tree is 0 levels deep, the tree with only one root node is 1 deep)
-             * @return
-             */
-            public int depth()
-            {
-                throw new NotImplementedException();
-            }
+        /**
+            * Returns the amount of nodes in the tree
+            * @return
+            */
+        public int count()
+        {
+            throw new NotImplementedException();
+        }
 
-            /**
-             * Returns the amount of nodes in the tree
-             * @return
-             */
-            public int count()
-            {
-                throw new NotImplementedException();
-            }
+        public void print()
+        {
+            throw new NotImplementedException();
+        }
 
-            public void print()
-            {
-                throw new NotImplementedException();
-            }
-
-            public void printInRange(int min, int max)
-            {
-                throw new NotImplementedException();
-            }
+        public void printInRange(int min, int max)
+        {
+            throw new NotImplementedException();
         }
     }
 }
