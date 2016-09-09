f5e40fd28dda16c7adf08c9b7ef1d6a7b155b308
Paul Wagener <mail@paulwagener.nl>
2016-09-09 19:05:00 +0200

    Template project week 3

---
+++ 3-Trees-Test/BinaryTreeTest.cs
@@ -0,0 +1,15 @@
+﻿using ALGA;
+using NUnit.Framework;
+
+namespace ALGA_test
+{
+    [Category("Binary Tree")]
+    public class BinaryTreeTest
+    {
+        [Test]
+        public void BinaryTreeInsertTest()
+        {
+
+        }
+    }
+}
